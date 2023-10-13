f0ff9bfbfe8e0807fb5a832134979853c8f7827c
Austin Sharman <asharman@generacgs.com>
2023-10-13 15:38:02 -0700

    draw simplex noise

--- src/Main.elm
+++ src/Main.elm
@@ -4,8 +4,39 @@ import Browser
 import Browser.Events exposing (onAnimationFrameDelta, onResize)
 import Canvas exposing (..)
 import Canvas.Settings exposing (..)
+import Canvas.Settings.Line exposing (lineWidth)
 import Color
 import Html exposing (Html)
+import Simplex exposing (PermutationTable)
+
+
+
+-- NOISE
+--Create a permutation table, using 42 as the seed
+
+
+permTable : PermutationTable
+permTable =
+    Simplex.permutationTableFromInt 42
+
+
+
+-- Create a function for 2D fractal noise
+
+
+noise : Float -> Float -> Float
+noise =
+    Simplex.fractal2d { scale = 10.0, steps = 5, stepSize = 3.0, persistence = 4.0 } permTable
+
+
+
+-- Create a 100x100 matrix of fractal noise.
+
+
+signal : Float -> List ( Float, Float )
+signal range =
+    List.range 0 (ceiling range)
+        |> List.map (\x -> ( toFloat x, noise (toFloat x) (toFloat 0) ))
 
 
 type alias Model =
@@ -21,9 +52,25 @@ init ( width, height ) =
 
 view : Model -> Html msg
 view { width, height } =
+    let
+        s =
+            signal width
+
+        head =
+            List.head s |> Maybe.withDefault ( 0, height / 2 )
+
+        rest =
+            List.tail s |> Maybe.withDefault []
+
+        adjusted =
+            Tuple.mapSecond (\y -> (y * height / 2) + height / 2)
+    in
     Canvas.toHtml ( round width, round height )
         []
-        [ shapes [ fill Color.black ] [ rect ( 0, 0 ) width height ] ]
+        [ shapes [ fill Color.black ] [ rect ( 0, 0 ) width height ]
+        , shapes [ stroke Color.white, lineWidth 2.0 ]
+            [ path (adjusted head) (List.map (lineTo << adjusted) rest) ]
+        ]
 
 
 type Msg
