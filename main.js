7ee37749e7ef19b5c05fd8e97a04601176fae5f0
Austin Sharman <asharman@generacgs.com>
2023-10-13 14:49:57 -0700

    add elm-canvas

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ import "./style.css"
 import { Elm } from "./src/Main.elm"
 
 const root = document.querySelector("#app")
-const app = Elm.Main.init({node: root});
+const app = Elm.Main.init({
+   node: root,
+   flags: [window.innerWidth, window.innerHeight]
+});
