461812215c67b7e5818aa880d4844d53f6a0e203
Austin Sharman <asharman@generacgs.com>
2023-10-15 11:35:51 -0600

    update base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import plugin from "vite-plugin-elm";
 
 export default defineConfig({
+  base: "/elm-rain/",
   plugins: [plugin()],
   build: { outDir: "./dist" },
 });
